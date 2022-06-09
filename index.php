03d4af2f5094db8d9cd86d561ae04e74c4b824a3
Florian Mabillon <mabillon.flo@gmail.com>
2022-06-09 14:45:22 +0200

    Ajout de commentaire dans le code

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// creation du tableau dans le HTML avec 3 colonnes(Nombre, Carré et Racine carré)
 echo '<table border>
         <tr>
             <th>Nombre</td>
@@ -6,8 +7,12 @@ echo '<table border>
             <th>Racine</td>
         </tr>';
 
-for ($i = 1; $i < 11; $i++){
+// Intégration du boucle FOR dans le tableau
 
+// Boucle FOR avec comme parametre: une initialisation à 1, i strictement inférieur à 11 afin d'avoir une liste de 1 jusqu'à 10, et une incrementation de i 
+
+for ($i = 1; $i < 11; $i++){
+// Affichage des résultat de i (Carré et Racine) dans le tableau
     echo '<tr>
             <td>'. $i . '</td>
             <td>'. $i*$i . '</td>
@@ -15,6 +20,7 @@ for ($i = 1; $i < 11; $i++){
     </tr>';
 }
 
+// Fermeture du tableau
 echo '</table>';
 
 
